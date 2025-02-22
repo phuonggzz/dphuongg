5a39fed13b4c67c533d52a80d846b91e20feef66
phuonggzz <139933122+phuonggzz@users.noreply.github.com>
2025-02-22 01:55:10 +0000

    First commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# dphuongg# dphuongg
---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.querySelector(".btn");
+    button.addEventListener("click", function () {
+        alert("Tải xuống CV thành công!");
+    });
+});
